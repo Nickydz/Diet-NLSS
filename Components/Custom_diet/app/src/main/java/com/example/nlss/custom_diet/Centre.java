822b1d54d6941b3bf483dbd4211bd458f3425d9b
Leo Thomson <leothomsonltc@gmail.com>
2017-02-10 22:08:45 +0530

    Custom-diet started

---
+++ Components/Custom_diet/app/src/main/java/com/example/nlss/custom_diet/Centre.java
@@ -0,0 +1,13 @@
+package com.example.nlss.custom_diet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Centre extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_centre);
+    }
+}
