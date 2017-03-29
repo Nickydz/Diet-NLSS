0ba39ad1bee12f6f17aeb4675b11e5936441c877
Nicky D'cruz <nickydcruz@yahoo.com>
2017-03-29 13:30:23 +0530

    Merge branch 'master' of https://github.com/Nickydz/Diet-NLSS
    # Conflicts:
    #	Components/LoginRegister/app/build.gradle
    #	Components/LoginRegister/app/src/main/AndroidManifest.xml
    #	Components/LoginRegister/build.gradle

---
+++ Components/LoginRegister/app/src/main/java/com/example/nickydcruz/loginregister/Notification.java
@@ -0,0 +1,164 @@
+package com.example.nickydcruz.loginregister;
+
+import android.app.TimePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Notification extends AppCompatActivity {
+    TextView brText;
+    TextView snText;
+    TextView lnText;
+    TextView snText1;
+    TextView dnText;
+
+    Calendar c = Calendar.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+        brText = (TextView) findViewById(R.id.brText);
+        snText = (TextView) findViewById(R.id.snText);
+        lnText = (TextView) findViewById(R.id.lnText);
+        snText1 = (TextView) findViewById(R.id.snText1);
+        dnText = (TextView) findViewById(R.id.dnText);
+        Button brButton = (Button) findViewById(R.id.brButton);
+        Button snButton = (Button) findViewById(R.id.snButton);
+        Button lnButton = (Button) findViewById(R.id.lnButton);
+        Button snButton1 = (Button) findViewById(R.id.snButton1);
+        Button dnButton = (Button) findViewById(R.id.dnButton);
+
+        setCurrentDateOnbrView();
+        setCurrentDateOndnView();
+        setCurrentDateOnlnView();
+        setCurrentDateOnsn1View();
+        setCurrentDateOnsnView();
+        brText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TimePickerDialog(Notification.this,new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hour, int min) {
+                        c.set(Calendar.HOUR_OF_DAY, hour);
+                        c.set(Calendar.MINUTE, min);
+                        setCurrentDateOnbrView();
+                    }
+                }, c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), true).show();
+            }
+        });
+
+//Snack
+        snText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TimePickerDialog(Notification.this,new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hour, int min) {
+                        c.set(Calendar.HOUR_OF_DAY, hour);
+                        c.set(Calendar.MINUTE, min);
+                        setCurrentDateOnsnView();
+                    }
+                }, c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), true).show();
+            }
+        });
+
+        //Lunch
+        lnText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TimePickerDialog(Notification.this,new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hour, int min) {
+                        c.set(Calendar.HOUR_OF_DAY, hour);
+                        c.set(Calendar.MINUTE, min);
+                        setCurrentDateOnlnView();
+                    }
+                }, c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), true).show();
+            }
+        });
+
+        //Snack1
+        snText1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TimePickerDialog(Notification.this,new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hour, int min) {
+                        c.set(Calendar.HOUR_OF_DAY, hour);
+                        c.set(Calendar.MINUTE, min);
+                        setCurrentDateOnsn1View();
+                    }
+                }, c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), true).show();
+            }
+        });
+
+        //Dinner
+        dnText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TimePickerDialog(Notification.this,new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hour, int min) {
+                        c.set(Calendar.HOUR_OF_DAY, hour);
+                        c.set(Calendar.MINUTE, min);
+                        setCurrentDateOndnView();
+                    }
+                }, c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), true).show();
+            }
+        });
+
+        brButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+    }
+
+
+//
+//    TimePickerDialog.OnTimeSetListener time =
+
+//    public void timeOnClick(View view){
+//
+//    }
+    public void setCurrentDateOnbrView(){
+        String timeFormat = "hh:mm a";
+        SimpleDateFormat stf = new SimpleDateFormat(timeFormat, Locale.US);
+        brText.setText(stf.format(c.getTime()));
+    }
+
+    public void setCurrentDateOnlnView(){
+        String timeFormat = "hh:mm a";
+        SimpleDateFormat stf = new SimpleDateFormat(timeFormat, Locale.US);
+        lnText.setText(stf.format(c.getTime()));
+    }
+    public void setCurrentDateOnsnView(){
+        String timeFormat = "hh:mm a";
+        SimpleDateFormat stf = new SimpleDateFormat(timeFormat, Locale.US);
+        snText.setText(stf.format(c.getTime()));
+    }
+    public void setCurrentDateOnsn1View(){
+        String timeFormat = "hh:mm a";
+        SimpleDateFormat stf = new SimpleDateFormat(timeFormat, Locale.US);
+        snText1.setText(stf.format(c.getTime()));
+    }
+    public void setCurrentDateOndnView(){
+        String timeFormat = "hh:mm a";
+        SimpleDateFormat stf = new SimpleDateFormat(timeFormat, Locale.US);
+        dnText.setText(stf.format(c.getTime()));
+    }
+
+
+}
